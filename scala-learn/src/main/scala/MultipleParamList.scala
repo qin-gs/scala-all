300e3f88cb279a1ae8d41caa1fbb4c4b3935f81b
qin-gs <3210968849@qq.com>
2023-05-31 22:25:31 +0800

    scala 基础

--- scala-learn/src/main/scala/MultipleParamList.scala
+++ scala-learn/src/main/scala/MultipleParamList.scala
@@ -3,15 +3,15 @@
  */
 object MultipleParamList {
 
-  def foldLeft[B](z: B)(op: (B, T) => B): B
-
-  def main(args: Array[String]): Unit = {
-    val numbers = List(1, 2, 3, 4, 5, 6, 7, 8, 9)
-    val res = numbers.foldLeft(0)((m, n) => m + n)
-    println(res)
-
-    numbers.foldLeft(0, {(m: Int, n: Int) => m + n})
-
-  }
+  // def foldLeft[B](z: B)(op: (B, T) => B): B
+  //
+  // def main(args: Array[String]): Unit = {
+  //   val numbers = List(1, 2, 3, 4, 5, 6, 7, 8, 9)
+  //   val res = numbers.foldLeft(0)((m, n) => m + n)
+  //   println(res)
+  //
+  //   // numbers.foldLeft(0, {(m: Int, n: Int) => m + n})
+  //
+  // }
 
 }
---
+++ scala-learn/src/main/scala/RegularExp.scala
@@ -0,0 +1,30 @@
+import scala.util.matching.Regex
+
+object RegularExp {
+
+  def main(args: Array[String]): Unit = {
+    val numberPattern: Regex = "[0-9]".r
+    numberPattern.findFirstMatchIn("awesome password") match {
+      case Some(_) => println("password ok")
+      case None => println("password must contain a number")
+    }
+
+    // 用括号来同时匹配多组正则表达式
+    val keyPattern: Regex = "([0-9a-zA-Z-#() ]+): ([0-9a-zA-Z-#() ]+)".r
+    val input: String =
+      """background-color: #A03300;
+        |background-image: url(img/header100.png);
+        |background-position: top center;
+        |background-repeat: repeat-x;
+        |background-size: 2160px 108px;
+        |margin: 0;
+        |height: 108px;
+        |width: 100%;""".stripMargin
+
+    for (patternMatch <- keyPattern.findAllMatchIn(input)) {
+      println(s"key: ${patternMatch.group(1)} value: ${patternMatch.group(2)}")
+    }
+
+
+  }
+}
